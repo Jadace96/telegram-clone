633c8a1b55d56197f73e4cf9e6c41fc8cf519d15
Jorge Daniel <jadace96@gmail.com>
2022-01-17 04:02:08 -0500

    Fix onPostUser

--- api/components/user/user-network.component.js
+++ api/components/user/user-network.component.js
@@ -7,15 +7,15 @@ const BASE_PATH = '/api/v1/user';
 const userRouter = express.Router();
 
 const onPostUser = async (req, res) => {
-  const newObject = new Parse.Object(className);
+  const newObject = new Parse.Object('Employees');
 
   try {
-    const data = await newObject.save({ ...dataToSave }, { ...options });
+    const data = await newObject.save({ ...req.body });
     console.log('Object created', data);
     res.status(201).send({ isSuccess: true, data: { ...req.body } });
   } catch (error) {
     console.error('Error while creating Object: ', error);
-    res.status(statusCode).send({ error: message });
+    res.status(500).send({ error });
   }
 };
 

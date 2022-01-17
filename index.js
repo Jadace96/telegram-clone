1157d691c74085041d3bea97beb1ac75b7f59caf
Jorge Daniel <jadace96@gmail.com>
2022-01-17 04:08:30 -0500

    Moved index.js to root project

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 import express from 'express';
 
 // utils
-import { injectMiddlewares } from './utils';
+import { injectMiddlewares } from './api/utils';
 
 // config
-import { config } from './config';
+import { config } from './api/config';
 
 // lib
-import { DBConnection } from './lib';
+import { DBConnection } from './api/lib';
 
 const server = express(); // to initialize express
 

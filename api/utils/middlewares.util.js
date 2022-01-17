dd014a3a399446d38149e3493f409b7dd3f592dd
Jorge Daniel <jadace96@gmail.com>
2022-01-17 01:27:52 -0500

    set CORS

--- api/utils/middlewares.util.js
+++ api/utils/middlewares.util.js
@@ -1,4 +1,5 @@
 // vendors
+import cors from 'cors';
 import path from 'path';
 import dotenv from 'dotenv';
 import express from 'express';
@@ -11,14 +12,33 @@ import { registerAppRoutes } from '../network';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const publicFolderPath = path.join(__dirname, '../public');
 
-const options = [
-  express.json(), // to parse body sended as JSON
-  express.urlencoded({ extended: false }), // to parse body sended as urlencoded
-];
+const setENV = () => {
+  dotenv.config({ path: path.join(__dirname, '../.env') });
+};
+
+const setCORS = (server) => {
+  const webLocalHost = 'http://localhost:3000/';
+  const corsOptions = [webLocalHost];
+  server.use(cors(corsOptions));
+};
+
+const setAllowedParseBodyOptions = (server) => {
+  const options = [
+    express.json(), // to parse body sended as JSON
+    express.urlencoded({ extended: false }), // to parse body sended as urlencoded
+  ];
+
+  options.forEach((option) => server.use(option));
+};
+
+const setStaticFoder = (server) => {
+  server.use('/api', express.static(publicFolderPath));
+};
 
 export const injectMiddlewares = (server) => {
-  dotenv.config({ path: path.join(__dirname, '../.env') });
-  options.forEach((option) => server.use(option));
-  server.use('/api', express.static(publicFolderPath));
+  setENV();
+  setCORS(server);
+  setAllowedParseBodyOptions(server);
+  setStaticFoder(server);
   registerAppRoutes(server);
 };
